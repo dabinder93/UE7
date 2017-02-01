6523702f389c28e36d5135ee20f97fa7606dcad7
Student <Student@VM-macOS-1012.local>
2017-02-01 15:19:08 +0100

    initial Commit

--- UE7/ViewController.swift
+++ UE7/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+@IBAction func scheduleBtn(_ sender: AnyObject) {
+    let center = UNUserNotificationCenter.current();
+    
+    let options: UNAuthorizationOptions = [.alert, .sound]
+    
+    var date1 = DateComponents()
+    date1.second = 60
+    let trigger = UNCalendarNotificationTrigger(dateMatching: date1 ,repeats: false)
+    
+    center.requestAuthorization(options: options) {
+        (granted, error) in
+        if !granted {
+            print("Something went wrong")
+        }
+    }
+    
+    center.getNotificationSettings { (settings) in
+        if settings.authorizationStatus != .authorized {
+            print("Notification not allowed")
+        }
+    }
+    
+    //get Current Time
+    let currentDateTime = Date()
+    let userCalendar = Calendar.current
+    let requestComponents: Set<Calendar.Component> = [
+        .hour,
+        .minute
+    ]
+    
+    let dateTimeComponents = userCalendar.dateComponents(requestComponents, from: currentDateTime)
+    
+    //Set Notification Content
+    let content = UNMutableNotificationContent()
+    content.title = "Star Wars App Notification"
+    content.body = "It's \(dateTimeComponents.hour):\(dateTimeComponents.minute)) your Character is: Obi Wan Kenobi "
+    
+    //identifier
+    let identifier = "SWLocalNotification"
+    
+    let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+    
+    center.add(request, withCompletionHandler: { (error) in
+        if error != nil {
+            // Something went wrong
+        }
+    })
+    
+}
 
 }
+
 
